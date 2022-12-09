9118d5c834abcd784075cbe330d9676b928a8a00
Taiki Mishima <32114124@stn.nitech.ac.jp>
2022-12-09 14:15:27 +0900

    データベースを利用できるように変更

--- module/recipe.py
+++ module/recipe.py
@@ -8,51 +8,46 @@ Created on Thu Oct 27 15:30:20 2022
 import requests
 import json
 import time
-import pandas as pd
+import sqlite3
+# import pandas as pd
 # from pprint import pprint
 
 class Recipe:
-    
+
     # 楽天レシピから検索ワードについてのレシピを取得する
     def get_recipe(self, replyText):
-        # 1. 楽天レシピのレシピカテゴリ一覧を取得する
+        # 楽天レシピのレシピカテゴリ一覧を取得する
         
-        res = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1069200340839550186')
-        
-        json_data = json.loads(res.text)
-        
-        parent_dict = {} # mediumカテゴリの親カテゴリの辞書
-        
-        df = pd.DataFrame(columns=['category1','category2','category3','categoryId','categoryName'])
-        
-        for category in json_data['result']['large']:
-            df = df.append({'category1':category['categoryId'],'category2':"",'category3':"",'categoryId':category['categoryId'],'categoryName':category['categoryName']}, ignore_index=True)
-        
-        for category in json_data['result']['medium']:
-            df = df.append({'category1':category['parentCategoryId'],'category2':category['categoryId'],'category3':"",'categoryId':str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, ignore_index=True)
-            parent_dict[str(category['categoryId'])] = category['parentCategoryId']
-        
-        for category in json_data['result']['small']:
-            df = df.append({'category1':parent_dict[category['parentCategoryId']],'category2':category['parentCategoryId'],'category3':category['categoryId'],'categoryId':parent_dict[category['parentCategoryId']]+"-"+str(category['parentCategoryId'])+"-"+str(category['categoryId']),'categoryName':category['categoryName']}, ignore_index=True)
-        
-        # 2. キーワードからカテゴリを抽出する
-        df_keyword = df.query('categoryName.str.contains('+ '"' + replyText + '"' + ')', engine='python')
-        
-        # pprint(df_keyword)
-        
-        # 3. 人気レシピを取得する
-        #df_recipe = pd.DataFrame(columns=['recipeId', 'recipeTitle', 'recipeUrl', 'foodImageUrl', 'recipeMaterial', 'recipeCost', 'recipeIndication', 'categoryId', 'categoryName'])
+        # カレントディレクトリにrecipe.dbがなければ、作成します。
+        # すでにTEST.dbが作成されていれば、recipe.dbに接続します。
+        dbname = './recipe.db'
+        conn = sqlite3.connect(dbname)
+
+        # 2.sqliteを操作するカーソルオブジェクトを作成
+        cur = conn.cursor()
+
+        # 3.テーブルのCreate文を実行(例ではpersonsテーブルを作成)
+        # テーブルが存在するか
+        cur.execute("SELECT COUNT(*) FROM sqlite_master"
+                "    WHERE TYPE = 'table' AND name = 'Recipes'")
+        row = cur.fetchone() # SQL文の実行結果をtupleで得る
+        if row[0] == 0:
+            self.create_db(conn, cur)
+
+        # データ検索
+        cur.execute("SELECT categoryId FROM Recipes WHERE categoryName=" + "'" + replyText + "'")
+
+        # 取得したデータはカーソルの中に入る
         recipesUrl = []
         recipesImg = []
         recipesTitle = []
         i = 0
-        for index, row in df_keyword.iterrows():
+        for row in cur:
+            # 終了判定
             if i > 0: 
                 break
             i += 1
-            time.sleep(3) # 連続でアクセスすると先方のサーバに負荷がかかるので少し待つのがマナー
-
-            url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1069200340839550186&categoryId='+row['categoryId']
+            url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1069200340839550186&categoryId='+ f"{row[0]}"
             res = requests.get(url)
 
             json_data = json.loads(res.text)
@@ -63,8 +58,13 @@ class Recipe:
                 recipesUrl.append(recipe['recipeUrl'])
                 recipesImg.append(recipe['foodImageUrl'])
                 recipesTitle.append(recipe['recipeTitle'])
+
+        # 5.データベースの接続を切断
+        cur.close()
+        conn.close()
+
         return recipesUrl, recipesImg, recipesTitle
-    '''
+
     def create_db(self, conn, cur):
         print("テーブルは存在しません")
         # テーブルを作成
@@ -98,8 +98,9 @@ class Recipe:
         # 4.データベースに情報をコミット
         conn.commit()
         time.sleep(3)
-'''
+
 #recipe = Recipe()
-#recipeUrl, imgUrl = recipe.get_recipe("さつまいも")
-#print(recipeUrl)
-#print(imgUrl)
+#url,img,title = recipe.get_recipe("さつまいも")
+#print(url)
+#print(img)
+#print(title)
